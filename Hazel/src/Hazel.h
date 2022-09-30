3761a6eb20e836f27053cdb26591d97df12e0aad
Leonsiuss <leonsiusgit@gmail.com>
2022-09-30 15:31:42 +0400

    Hazel_API & EntryPoint

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// For use Only Hazel application (SandboxApp)
+
+#include "Hazel/Application.h"
+
+
+/********  Entry Point  **************/
+#include "Hazel/EntryPoint.h" // we need, because -- int main() -- function is in EntryPoint.h
